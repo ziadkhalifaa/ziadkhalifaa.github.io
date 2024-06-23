aecadb1a206ebc3ca8a481322ccfc67dcc944856
ziadkhalifaa <130990877+ziadkhalifaa@users.noreply.github.com>
2024-06-23 22:46:13 +0300

    Add files via upload
    initial commit

---
+++ all.js
@@ -0,0 +1,26 @@
+        var button = document.createElement("button");
+
+        button.innerText = "Download APk for this Web";
+
+        button.classList.add ("styled-button");
+
+        document.getElementById("button-container").appendChild(button);
+
+        var url = "https://download938.mediafire.com/ie1iujw9j1fgcb07ON4M0mLq6ithHc3w6i5L6klPLI9lNbiAXnrNg4r3WM_YsXMtdgsG4k-vHoTfBLc2MJssWHIt53ozeFLcS6BbhxUkrQQgGao6ekAyvP2KutYsU-DDBZKl29Ugv_r1_D3R1OwRKdt2zl5ph0MlYGMYkqQopyiJIaXU/2ozjqdf2pogihuh/Robot+BST_1.0.apk";
+
+        button.onclick = function() {
+            window.location.href = url;
+        };
+    
+
+
+    function gosign() {
+        window.location.href = "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fmyaccount.google.com%2Fnotifications%2Feid%2F608551449735760836%3Forigin%3D3%26continue%3Dhttps%253A%252F%252Fmyaccount.google.com%252F&ec=GAlAwAE&hl=ar&service=accountsettings&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S1409638188%3A1715170784012749&theme=mn&ddm=0";
+    } ;
+
+
+    function redirectToURL() {
+        window.location.href = "https://firebasestorage.googleapis.com/v0/b/nour-6fd0d.appspot.com/o/Robot%2Fordar%2FCreate%20a%20request.html?alt=media&token=780e5c1c-337c-47f8-b19f-1ccbef012b60";
+    };
+
+   
